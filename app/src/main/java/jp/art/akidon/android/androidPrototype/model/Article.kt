a44d7edfcc35d3fb1dd7d217631a5b1096b37c0d
Akihiro Matsuyama <a@art.jp>
2022-02-12 19:27:25 +0900

    Article作成

---
+++ app/src/main/java/jp/art/akidon/android/androidPrototype/model/Article.kt
@@ -0,0 +1,6 @@
+package jp.art.akidon.android.androidPrototype.model
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
